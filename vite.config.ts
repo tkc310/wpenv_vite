ae8094324fc25d7d6c4a7b105f264a9d2ef13d65
Takashi Sato <takashisato.develop@gmail.com>
2024-11-17 16:40:43 +0900

    chore: vite導入

---
+++ vite.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig, UserConfig } from 'vite';
+import FullReload from 'vite-plugin-full-reload';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve, relative, extname } from "path";
+import { globSync } from "glob";
+import { fileURLToPath } from "node:url";
+
+const root = resolve(__dirname, "src");
+
+// @refs: https://vitejs.dev/config/
+const config = (): UserConfig => ({
+  root,
+  base: "./",
+  server: {
+    host: true,
+    cors: true,
+    strictPort: true,
+    port: 3000,
+    hmr: {
+      host: 'localhost'
+    }
+  },
+  preview: {
+    host: true,
+    port: 3000
+  },
+  build: {
+    outDir: './dist',
+    assetsDir: './src',
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: {
+        "styles/main.css": "src/styles/main.scss",
+        // @see: https://rollupjs.org/configuration-options/#input
+        ...Object.fromEntries(
+          globSync(["src/scripts/**/*.ts"]).map((file) => [
+            relative(
+              "src/scripts",
+              file.slice(0, file.length - extname(file).length),
+            ),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+        ),
+      },
+      output: {
+        entryFileNames: "scripts/[name].js",
+        chunkFileNames: "scripts/chunk.[name].js",
+        assetFileNames: "[name].[ext]",
+      }
+    }
+  },
+  plugins: [
+    FullReload(['./wordpress/**/*.php'], { root: __dirname }),
+    tsconfigPaths(),
+  ]
+});
+
+export default defineConfig(() => config());
--- wordpress/themes/example/index.php
+++ wordpress/themes/example/index.php
@@ -4,9 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Title</title>
+  <?php 
+    if (IS_DEVELOP) {
+      $root = "http://localhost:3000";
+      $css_ext = "scss";
+      $js_ext = "ts";
+      echo '<script type="module" src="http://localhost:3000/@vite/client"></script>';
+    } else {
+      $root = get_template_directory_uri();
+      $css_ext = "css";
+      $js_ext = "js";
+    } 
+  ?>
+  <link rel="stylesheet" href="<?php echo $root;?>/styles/main.<?php echo $css_ext?>">
+  <script src="<?php echo $root;?>/scripts/main.<?php echo $js_ext?>" type="module"></script>
 </head>
 <body>
   <h1>テスト</h1>
+  <div class="foo">
+    <p class="bar">hoge</p>
+  </div>
 </body>
 </html>
-
