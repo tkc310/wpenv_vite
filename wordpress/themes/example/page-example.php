57c320add93e0be5136c710acae4a6a78a62fa92
tkc310 <takashisato.develop@gmail.com>
2025-03-08 15:56:07 +0900

    feat: #2 固定ページ

---
+++ wordpress/themes/example/page-example.php
@@ -0,0 +1,18 @@
+<?php
+/* Template Name: example */
+?>
+
+<?php get_header(); ?>
+
+<h2 class="font-bold text-xl"><?php the_title(); ?></h2>
+<div class="foo">
+  <p class="bar"><?php the_content(); ?></p>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos exercitationem nam, aliquam saepe perferendis eligendi facilis provident eaque doloribus dolor! Autem quos repudiandae incidunt exercitationem nihil aliquid vero, nobis velit!</p>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos exercitationem nam, aliquam saepe perferendis eligendi facilis provident eaque doloribus dolor! Autem quos repudiandae incidunt exercitationem nihil aliquid vero, nobis velit!</p>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos exercitationem nam, aliquam saepe perferendis eligendi facilis provident eaque doloribus dolor! Autem quos repudiandae incidunt exercitationem nihil aliquid vero, nobis velit!</p>
+</div>
+
+<?php get_footer(); ?>
---
+++ wordpress/themes/example/page.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: default */
+?>
+
+<?php get_header(); ?>
+
+<h2 class="font-bold text-xl"><?php the_title(); ?></h2>
+<div class="foo">
+  <p class="bar"><?php the_content(); ?></p>
+
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos exercitationem nam, aliquam saepe perferendis eligendi facilis provident eaque doloribus dolor! Autem quos repudiandae incidunt exercitationem nihil aliquid vero, nobis velit!</p>
+</div>
+
+<?php get_footer(); ?>
