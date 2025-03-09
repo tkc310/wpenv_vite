ad8d31c249d01300dcaae9929f11592d18f2e328
tkc310 <takashisato.develop@gmail.com>
2025-03-09 14:44:31 +0900

    feat: #9 カスタムフィールド

--- wordpress/themes/example/functions.php
+++ wordpress/themes/example/functions.php
@@ -1,5 +1,10 @@
 <?php
 
+add_action('init', 'action_init');
+function action_init()
+{
+  init_post_type();
+}
 function init_post_type()
 {
   $supports = [
@@ -22,21 +27,50 @@ function init_post_type()
   );
 }
 
-function action_init()
-{
-  init_post_type();
-}
-
+add_action('after_setup_theme', 'action_after');
 function action_after()
 {
   add_theme_support('post-thumbnails'); // アイキャッチ画像を有効
 }
 
+add_action('pre_get_posts', 'action_pre_get_posts');
 function action_pre_get_posts($query)
 {
   $query->set('posts_per_page', 1); // 1ページ毎の表示件数
 }
 
-add_action('init', 'action_init');
-add_action('after_setup_theme', 'action_after');
-add_action('pre_get_posts', 'action_pre_get_posts');
+// カスタムフィールド
+add_action('add_meta_boxes', 'add_foo_fields');
+function add_foo_fields()
+{
+  add_meta_box(
+    'foo', // 表示される入力ボックスのHTMLのID
+    'Foo', // ラベル
+    'insert_foo_fields', // 表示する内容を作成する関数名
+    'page', // , 投稿タイプ (post | page)
+    'normal' // 位置 (normal | side | advanced)
+  );
+}
+function insert_foo_fields()
+{
+  global $post;
+
+  echo 'Foo: <input type="text" name="foo" value="' .
+    get_post_meta($post->ID, 'foo', true) .
+    '" size="50" /><br>';
+}
+
+add_action('save_post', 'save_foo_fields');
+function save_foo_fields($post_id)
+{
+  // 自動保存を除外
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+    return;
+  }
+
+  if (!empty($_POST['foo'])) {
+    update_post_meta($post_id, 'foo', $_POST['foo']);
+  } else {
+    delete_post_meta($post_id, 'foo');
+  }
+}
--- wordpress/themes/example/page-sample.php
+++ wordpress/themes/example/page-sample.php
@@ -6,6 +6,13 @@
 
 <h2 class="font-bold text-xl"><?php the_title(); ?></h2>
 <div class="foo">
+  <?php
+  $foo = get_post_meta(get_the_ID(), 'foo', true);
+  if (!empty($foo)): ?>
+    <h3><?php echo esc_html($foo); ?></h3>
+  <?php endif;
+  ?>
+
   <p class="bar"><?php the_content(); ?></p>
 
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos exercitationem nam, aliquam saepe perferendis eligendi facilis provident eaque doloribus dolor! Autem quos repudiandae incidunt exercitationem nihil aliquid vero, nobis velit!</p>
